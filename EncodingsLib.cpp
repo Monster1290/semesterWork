442b4ab8167f23cd49d0e709ccc27e6298472bd8
Monster1290 <besedin_alexsandr_1996@mail.ru>
2016-04-28 15:46:20 +0500

    Теперь шифр Цезаря учитывает регистр

--- EncodingsLib.cpp
+++ EncodingsLib.cpp
@@ -13,14 +13,20 @@ using namespace std;
 
 string Encoding::CaesarEncrypt(string s, unsigned short int key)
 {
-    transform(s.begin(), s.end(), s.begin(), ::toupper);
     for (int i = 0; i < s.length(); i++)
     {
         if (isalpha(s[i]))
         {
-            s[i] += key;
-            if (s[i] > 'Z') s[i] += -'Z' + 'A' - 1;
-            if (s[i] < 'A') s[i] += -'A' + 'Z' + 1;
+            uint16_t newSymbol = s[i] + key;
+            if (s[i] <= 'Z' && s[i] >= 'A') {
+                if (newSymbol > 'Z') newSymbol += -'Z' + 'A' - 1;
+                if (newSymbol < 'A') newSymbol += -'A' + 'Z' + 1;
+            }
+            if (s[i] <= 'z' && s[i] >= 'a') {
+                if (newSymbol > 'z') newSymbol += -'z' + 'a' - 1;
+                if (newSymbol < 'a') newSymbol += -'a' + 'z' + 1;
+            }
+            s[i] = newSymbol;
         }
     }
     return s;
