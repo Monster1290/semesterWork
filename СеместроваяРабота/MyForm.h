24a2187925220e48ab1e65d0ddd33ec7af38305b
NastyaMittseva <nastya_mittseva@mail.ru>
2016-05-08 10:50:09 +0500

    Создание интерфейса

--- СеместроваяРабота/MyForm.h
+++ СеместроваяРабота/MyForm.h
@@ -34,6 +34,16 @@ namespace СеместроваяРабота {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^  label3;
+	protected: 
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::TextBox^  textBox3;
 
 	private:
 		/// <summary>
@@ -48,11 +58,128 @@ namespace СеместроваяРабота {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Garamond", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->label3->ForeColor = System::Drawing::SystemColors::Desktop;
+			this->label3->Location = System::Drawing::Point(316, 9);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(135, 27);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"Шифратор";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(391, 72);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(353, 244);
+			this->textBox1->TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(25, 72);
+			this->textBox2->Multiline = true;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(353, 243);
+			this->textBox2->TabIndex = 5;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Garamond", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(159, 47);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(60, 21);
+			this->label1->TabIndex = 6;
+			this->label1->Text = L"Текст";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Garamond", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(471, 47);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(210, 21);
+			this->label2->TabIndex = 7;
+			this->label2->Text = L"Зашифрованный текст";
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Garamond", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(97, 327);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(147, 31);
+			this->button1->TabIndex = 9;
+			this->button1->Text = L"Шифр Цезаря";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Garamond", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->button2->Location = System::Drawing::Point(97, 366);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(147, 34);
+			this->button2->TabIndex = 10;
+			this->button2->Text = L"Шифр Виженера";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Garamond", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(352, 334);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(52, 18);
+			this->label5->TabIndex = 13;
+			this->label5->Text = L"Ключ:";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(321, 355);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(110, 20);
+			this->textBox3->TabIndex = 14;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(769, 417);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label3);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
