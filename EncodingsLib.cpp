a7d2ffc6ddd0aaf41d0f3a7e30e0fb9b81bf9ead
NastyaMittseva <nastya_mittseva@mail.ru>
2016-05-08 15:16:06 +0500

    Теперь Шифр Цезаря работает и с большими значениями ключа

--- EncodingsLib.cpp
+++ EncodingsLib.cpp
@@ -14,30 +14,44 @@ using namespace std;
 
 string Encoding::CaesarEncrypt(string s, unsigned short int key)
 {
-    for (int i = 0; i < s.length(); i++)
-    {
-        if (isalpha(s[i]))
-        {
-            char newSymbol = s[i] + key;
-            if (s[i] <= 'Z' && s[i] >= 'A') {
-                if (newSymbol > 'Z') newSymbol += -'Z' + 'A' - 1;
-                if (newSymbol < 'A') newSymbol += -'A' + 'Z' + 1;
-            }
-            if (s[i] <= 'z' && s[i] >= 'a') {
-                if (newSymbol > 'z') newSymbol += -'z' + 'a' - 1;
-                if (newSymbol < 'a') newSymbol += -'a' + 'z' + 1;
-            }
-			if ((s[i] >= 'А') && (s[i] <= 'Я')) {
-				if (newSymbol > 'Я')  newSymbol += -'Я' + 'А' - 1;
-			    if (newSymbol < 'А')  newSymbol += -'А' + 'Я' + 1;
+   unsigned char newEnglishSymbol; //т.к. char только положительные
+   char newRussianSymbol; //т.к. char могут быть и отрицательными
+   for (int i = 0; i < s.length(); i++) {
+		if (isalpha(s[i])) {          //если символ - буква
+			if (((s[i] >= 'A') && (s[i] <= 'Z'))||((s[i] >= 'a') && (s[i] <= 'z'))){ //английский язык
+				while ((key>27)||(key<-27)){  
+					if (key>27) key=key-26;
+					if (key<-27) key=key+26;
+				}
+				newEnglishSymbol = s[i] + key;
+				if ((s[i] >= 'a') && (s[i] <= 'z')) {
+					if (newEnglishSymbol > 'z')  newEnglishSymbol += -'z' + 'a' - 1; 
+					 if (newEnglishSymbol < 'a')  newEnglishSymbol += -'a' + 'z' + 1; 
+				 }
+				if ((s[i] >= 'A') && (s[i] <= 'Z')) {
+					if (newEnglishSymbol > 'Z') newEnglishSymbol += -'Z' + 'A' - 1;
+					if (newEnglishSymbol < 'A') newEnglishSymbol += -'A' + 'Z' + 1;
+				}
+				s[i] = newEnglishSymbol;
 			}
-			if ((s[i] >= 'а') && (s[i] <= 'я')) {
-			    if (newSymbol > 'я')  newSymbol += -'я' + 'а' - 1;
-				if (newSymbol < 'а')  newSymbol += -'а' + 'я' + 1;
+			if (((s[i] >= 'А') && (s[i] <= 'Я'))||((s[i] >= 'а') && (s[i] <= 'я'))){   //русский язык
+				while ((key>33)||(key<-33)){
+					if (key>33) key=key-32;
+					if (key<-33) key=key+32;
+				}
+				newRussianSymbol = s[i] + key;
+				if ((s[i] >= 'А') && (s[i] <= 'Я')) {
+					if (newRussianSymbol > 'Я') newRussianSymbol += -'Я' + 'А' - 1;
+					if (newRussianSymbol < 'А') newRussianSymbol += -'А' + 'Я' + 1;
+				}
+				if ((s[i] >= 'а') && (s[i] <= 'я')) {
+					if (newRussianSymbol > 'я') newRussianSymbol+= -'я' + 'а' - 1;
+					if (newRussianSymbol < 'а') newRussianSymbol += -'а' + 'я' + 1;
+				}	
+				s[i] = newRussianSymbol;
 			}
-            s[i] = newSymbol;
-        }
-    }
+		}
+	}
     return s;
 }
 
